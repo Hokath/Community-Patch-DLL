c41f62b5ae29d9169e1aa8c7e480cd07a51eedf1
Tom <thomas.sayer@btinternet.com>
2026-02-06 21:56:35 +0000

    compiler wants this to be const

--- CvGameCoreDLL_Expansion2/CvCity.cpp
+++ CvGameCoreDLL_Expansion2/CvCity.cpp
@@ -14903,7 +14903,7 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bFirst, 
 		// Building Bonuses from Accomplishments
 		if (!pBuildingInfo->GetBonusFromAccomplishments().empty())
 		{
-			auto& mBonusFromAccomplishments = pBuildingInfo->GetBonusFromAccomplishments();
+			const auto& mBonusFromAccomplishments = pBuildingInfo->GetBonusFromAccomplishments();
 			for (auto it = mBonusFromAccomplishments.begin(); it != mBonusFromAccomplishments.end(); ++it)
 			{
 				int iNumAccomplishmentCompleted =
